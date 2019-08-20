79cfe784612fdbb4816c9fc3fc7222c845a3268f
shaukhk01 <shaukhk01@gmail.com>
2019-08-20 11:36:40 +0530

    dict logic

--- pro62.py
+++ pro62.py
@@ -5,3 +5,17 @@ def main():
     for s2 in s1:
         print(s2)
 main()
+print('-'*50)
+
+def main():
+    contact = dict()
+    no_emp = int(input('Enter a no of Emp:'))
+    while no_emp>0:
+        name = input('Enter Emp Name')
+        p_No = input('Enter Emp Phone Number')
+        email= input('Enter Emp Email.')
+        contact[name] = p_No.split()+email.split()
+        no_emp -=1
+    for data in contact:
+        print(data,'\t',contact[data])
+main()
